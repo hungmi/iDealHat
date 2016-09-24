74d7143699d98e99bbf142fba29e65cb6d3473d1
hungmi <hungmihungmi@gmail.com>
2016-09-24 22:22:33 +0800

    js RWD for questions#new

---
+++ app/assets/javascripts/init.js
@@ -0,0 +1,20 @@
+$(document).on("turbolinks:load", function(){
+  if ($("i#mobile-detector").css("display") == "none"){
+    window.mobile = true;
+  }
+})
+
+$(document).on("turbolinks:load", function(){
+  if (window.mobile){
+    $("#iphone").css("height", $(window).height()) 
+  } else {
+    $("#iphone").css("width", "100%")
+    var base = $("#iphone").outerWidth();
+    $("#iphone").css("height", base/1081*2281)
+    $("#questions").css("height", base/1081*2281)
+    $("#transformed_questions").css("left", base/1081*53)
+    $("#transformed_questions").css("top", base/1081*267)
+    $("#transformed_questions").css("width", base/1081*974)
+    $("#transformed_questions").css("height", base/1081*1720)
+  }
+})
--- app/assets/javascripts/question_transformations.js
+++ app/assets/javascripts/question_transformations.js
@@ -45,17 +45,3 @@ $(document).on("input", "#questions", function(){
     }
   })
 })
-
-// 按下選項會有勾勾表示選了
-$(document).on("click", ".question_option:not('.note')", function(){
-  $(this).parents(".question_options_wrapper").find(".glyphicon-ok").remove()
-  $(this).html($(this).html() + " <i class='glyphicon glyphicon-ok' style='color: green;'/>")
-})
-
-// 有 note 這個 class 代表此問題要問文字答案
-$(document).on("click", ".question_option.note", function(){
-  $(this).html(function() { return $(this).html().replace($(this).data("note"), "") })
-  var note = prompt("請輸入答案", $(this).data("note"))
-  $(this).data("note", note)
-  $(this).html(function() { return $(this).html() + "\n" + $(this).data("note") })
-})
